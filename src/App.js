a49ca9ee6d0adb9201de414272fdc21c87d920ec
gianluca galota <gianluca.galota@gmail.com>
2021-12-30 23:41:45 +0000

    reactrouter added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import { render } from "react-dom";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <h1>Adopt Me!</h1>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
+
   );
 };
 
---
+++ src/Details.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Details = () => {
+  return (
+    <div>
+      <h2>Helloooo!</h2>
+    </div>
+
+  )
+};
+
+export default Details;
